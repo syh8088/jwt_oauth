6c55aee24893e1c0a8ae8171bcc43a14ae2e884b
syh8088 <syh8088@gmail.com>
2020-11-28 16:20:57 +0900

    FilterSecurityInterceptor
    resource 서버로 옮김

--- src/main/java/com/authorization/common/config/SecurityConfig.java
+++ src/main/java/com/authorization/common/config/SecurityConfig.java
@@ -3,26 +3,16 @@ package com.authorization.common.config;
 import com.authorization.common.config.filter.JwtAuthenticationFilter;
 import com.authorization.common.config.handler.CustomAuthenticationProvider;
 import com.authorization.common.config.handler.CustomAuthenticationSuccessHandler;
-import com.authorization.common.config.metadatasource.UrlFilterInvocationSecurityMetadataSource;
 import com.authorization.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.vote.AffirmativeBased;
-import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -55,8 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers( "/oauth/token").permitAll()
                 .antMatchers("/tokens").permitAll()
             .and()
-                .addFilterBefore(authenticationFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
+                .addFilterBefore(authenticationFilter(), BasicAuthenticationFilter.class);
     }
 
     @Bean
@@ -70,30 +59,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public FilterSecurityInterceptor customFilterSecurityInterceptor() throws Exception {
-
-        //PermitAllFilter permitAllFilter = new PermitAllFilter(permitAllResources);
-        FilterSecurityInterceptor filterSecurityInterceptor = new FilterSecurityInterceptor();
-
-        filterSecurityInterceptor.setSecurityMetadataSource(urlFilterInvocationSecurityMetadataSource());
-        filterSecurityInterceptor.setAccessDecisionManager(affirmativeBased());
-        filterSecurityInterceptor.setAuthenticationManager(authenticationManagerBean());
-
-        return filterSecurityInterceptor;
-    }
-
-    private AccessDecisionManager affirmativeBased() {
-        AffirmativeBased affirmativeBased = new AffirmativeBased(getAccessDecisionVoters());
-        return affirmativeBased;
-    }
-
-    private List<AccessDecisionVoter<? extends Object>> getAccessDecisionVoters() {
-        return Arrays.asList(new RoleVoter());
-    }
-
-    @Bean
-    public FilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() throws Exception {
-        return new UrlFilterInvocationSecurityMetadataSource();
-    }
 }
